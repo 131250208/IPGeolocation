fc57c09eed6419203a9b7f58a16cc863a0434228
wycheng <wychengpublic@163.com>
2018-12-28 23:54:42 +0800

    -

--- Experiments/exp1_planetlab.py
+++ Experiments/exp1_planetlab.py
@@ -17,45 +17,75 @@ def map_ip_2_geolocation_st_1():
 
 
 if __name__ == "__main__":
-    samples = json.load(open("../Sources/experiments/samples_planetlab_us.json", "r", encoding="utf-8"))
-    for s in pyprind.prog_bar(samples):
-        ip = s["ip"]
-        est_ipip = geoloc_commercial_db.ip_geolocation_ipip(ip)
-        est_ipplus = geoloc_commercial_db.ip_geolocation_ipplus360(ip)
-        est_ipinfo = geoloc_commercial_db.ip_geolocation_ipinfo(ip)
-        est_geolite2 = geoloc_commercial_db.ip_geolocation_geolite2(ip)
-        est_ipstack = geoloc_commercial_db.ip_geolocation_ipstack(ip)
-
-        s["est_ipip"] = est_ipip
-        s["est_ipplus"] = est_ipplus
-        s["est_ipinfo"] = est_ipip
-        s["est_geolite2"] = est_geolite2
-        s["est_ipstack"] = est_ipstack
-    json.dump(samples, open("../Sources/experiments/samples_planetlab_us.json", "w", encoding="utf-8"))
-
-
     # samples = json.load(open("../Sources/experiments/samples_planetlab_us.json", "r", encoding="utf-8"))
-    # count = 0
-    # sample_fin = []
-    # error_list = []
-    # for sample in samples:
-    #     if sample[strings.KEY_STATUS] == enumeration.SAMPLE_STATUS_FIN:
-    #         # if sample["dis_coarse_2_ground"] > 50000:
-    #         #     continue
-    #         med_one = geo_distance_calculator.get_geodistance_btw_2coordinates(sample["longitude"], sample["latitude"], sample[strings.KEY_ESTIMATED_COORDINATE]["longitude"], sample[strings.KEY_ESTIMATED_COORDINATE]["latitude"])
+    # for s in pyprind.prog_bar(samples):
+    #     ip = s["ip"]
+    #     est_ipip = geoloc_commercial_db.ip_geolocation_ipip(ip)
+    #     est_ipplus = geoloc_commercial_db.ip_geolocation_ipplus360(ip)
+    #     est_ipinfo = geoloc_commercial_db.ip_geolocation_ipinfo(ip)
+    #     est_geolite2 = geoloc_commercial_db.ip_geolocation_geolite2(ip)
+    #     est_ipstack = geoloc_commercial_db.ip_geolocation_ipstack(ip)
     #
-    #         sample_fin.append(sample)
-    #         count += 1
-    #         error_list.append(med_one)
-    #
-    #         print("ip: {}, org: {} error_dis: {}".format(sample["ip"], sample["organization"], med_one))
-    # print(np.median(error_list))
-    # print(len(sample_fin))
-    #
+    #     s["est_ipip"] = est_ipip
+    #     s["est_ipplus"] = est_ipplus
+    #     s["est_ipinfo"] = est_ipinfo
+    #     s["est_geolite2"] = est_geolite2
+    #     s["est_ipstack"] = est_ipstack
+    # json.dump(samples, open("../Sources/experiments/samples_planetlab_us.json", "w", encoding="utf-8"))
+
+
+    samples = json.load(open("../Sources/experiments/samples_planetlab_us.json", "r", encoding="utf-8"))
+    count = 0
+    sample_fin = []
+    med_list_one = []
+    med_list_ipplus = []
+    med_list_ipinfo = []
+    med_list_ipip = []
+    med_list_ipstack = []
+    med_list_geolite2 = []
+    for sample in samples:
+        if sample[strings.KEY_STATUS] == enumeration.SAMPLE_STATUS_FIN:
+            # if sample["dis_coarse_2_ground"] > 50000:
+            #     continue
+            ground_lon = sample["longitude"]
+            ground_lat = sample["latitude"]
+            lon_one = sample[strings.KEY_ESTIMATED_COORDINATE]["longitude"]
+            lat_one = sample[strings.KEY_ESTIMATED_COORDINATE]["latitude"]
+            lon_ipplus = sample["est_ipplus"]["longitude"]
+            lat_ipplus = sample["est_ipplus"]["latitude"]
+            lon_ipip = sample["est_ipip"]["longitude"]
+            lat_ipip = sample["est_ipip"]["latitude"]
+            lon_ipinfo = sample["est_ipinfo"]["longitude"]
+            lat_ipinfo = sample["est_ipinfo"]["latitude"]
+            lon_ipstack = sample["est_ipstack"]["longitude"]
+            lat_ipstack = sample["est_ipstack"]["latitude"]
+            lon_geolite2 = sample["est_geolite2"]["longitude"]
+            lat_geolite2 = sample["est_geolite2"]["latitude"]
+            err_one = geo_distance_calculator.get_geodistance_btw_2coordinates(ground_lon, ground_lat, lon_one, lat_one)
+            err_ipplus = geo_distance_calculator.get_geodistance_btw_2coordinates(ground_lon, ground_lat, lon_ipplus, lat_ipplus)
+            err_ipip = geo_distance_calculator.get_geodistance_btw_2coordinates(ground_lon, ground_lat, lon_ipip, lat_ipip)
+            err_ipinfo = geo_distance_calculator.get_geodistance_btw_2coordinates(ground_lon, ground_lat, lon_ipinfo, lat_ipinfo)
+            err_geolite2 = geo_distance_calculator.get_geodistance_btw_2coordinates(ground_lon, ground_lat, lon_geolite2, lat_geolite2)
+            err_ipstack = geo_distance_calculator.get_geodistance_btw_2coordinates(ground_lon, ground_lat, lon_ipstack, lat_ipstack)
+
+            sample_fin.append(sample)
+            count += 1
+            med_list_one.append(err_one)
+            med_list_ipplus.append(err_ipplus)
+            med_list_ipip.append(err_ipip)
+            med_list_ipstack.append(err_ipstack)
+            med_list_ipinfo.append(err_ipinfo)
+            med_list_geolite2.append(err_geolite2)
+
+            print("ip: {}, org: {} \n"
+                  "error_one: {:.1f}, err_ipplus: {:.1f}, err_ipip: {:.1f}, err_ipinfo: {:.1f}, err_ipstack: {:.1f}, err_geolite2: {:.1f}".format(sample["ip"], sample["organization"], err_one, err_ipplus, err_ipip, err_ipinfo, err_ipstack, err_geolite2))
+    print(np.median(med_list_one))
+    print(len(sample_fin))
+
     # import numpy as np
     # import matplotlib.pyplot as plt
     # samples = []
-    # for ind, e in enumerate(error_list):
+    # for ind, e in enumerate(med_list_one):
     #     if e > 18000:
     #         continue
     #     samples.append(e)
